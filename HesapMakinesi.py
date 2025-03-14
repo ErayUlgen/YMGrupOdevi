9f2c0ec26992f29c0c689e8214a5bda8a0405047
Berke Çakıroğlu <berkecakiroglu35@gmail.com>
2025-03-14 17:32:19 +0300

    Çıkarma işlemini ekler

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -1,1 +1,9 @@
 print("Merhaba Dünya")
+def cikarma(*sayilar):
+    if not sayilar:
+        return 0
+    sonuc = sayilar[0]
+    for sayi in sayilar[1:]:
+        sonuc -= sayi
+    return sonuc
+
