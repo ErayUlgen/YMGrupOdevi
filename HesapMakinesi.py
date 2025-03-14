081345d6edc9004298ed22cb4b91e1e5879b695d
enesilbay <enesilbayy@gmail.com>
2025-03-14 18:12:26 +0300

    karekök işlemini ekler

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -1,6 +1,6 @@
 
 def carpma(*args):
-    sonuc = 1
+    sc = 1
     for sayi in args:
         sonuc *= sayi
     return sonuc
@@ -15,4 +15,15 @@ def cikarma(*sayilar):
 
 def topla(*sayilar):
     return sum(sayilar)
+
+import math
+
+def karekok_al(*args):
+    for sayi in args:
+        if sayi >= 0:
+            print(f"{sayi} sayısının karekökü: {math.sqrt(sayi)}")
+        else:
+            print(f"{sayi} negatif bir sayı olduğu için karekökü alınamaz.")
+
+
 
